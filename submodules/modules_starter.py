ca2432a5165a88843f68a4eec7e8022a5cf0f24b
kou1046 <iwashiro0517@yahoo.co.jp>
2023-05-17 23:36:42 +0900

    コメントアウトの消去

--- submodules/modules_starter.py
+++ submodules/modules_starter.py
@@ -19,13 +19,13 @@ for container_input_path in container_input_paths:
     container_out_dir = f"/app/outputs/{video_name}"
 
     # OPENPOSE
-    # os.makedirs(
-    #     os.path.join(__file__, "..", "outputs", video_name, OPENPOSE_OUTPUT_DIR_NAME),
-    #     exist_ok=True,
-    # )
-    # os.system(
-    #     f'docker compose run --rm openpose /bin/bash -c "./build/examples/openpose/openpose.bin --video {container_input_path} --render_pose 0 --display 0 --write_json {container_out_dir}/{OPENPOSE_OUTPUT_DIR_NAME}"'
-    # )
+    os.makedirs(
+        os.path.join(__file__, "..", "outputs", video_name, OPENPOSE_OUTPUT_DIR_NAME),
+        exist_ok=True,
+    )
+    os.system(
+        f'docker compose run --rm openpose /bin/bash -c "./build/examples/openpose/openpose.bin --video {container_input_path} --render_pose 0 --display 0 --write_json {container_out_dir}/{OPENPOSE_OUTPUT_DIR_NAME}"'
+    )
 
     # DEEPSORT
     os.makedirs(
