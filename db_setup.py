f5180f941ac644ca84a784c3cbaa5c953aa5abfb
kou1046 <iwashiro0517@yahoo.co.jp>
2023-11-27 19:22:01 +0900

    [Change]: 環境ファイルの変更

--- db_setup.py
+++ db_setup.py
@@ -16,19 +16,13 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "3306",
     },
-    "2023": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ["MYSQL_DATABASE"],
-        "USER": os.environ["MYSQL_USER"],
-        "PASSWORD": os.environ["MYSQL_PASSWORD"],
-        "HOST": "localhost",
-        "PORT": "3307",
-    },
 }
 
 INSTALLED_APPS = ("api",)
 
+TIME_ZONE = "Asia/Tokyo"
 SECRET_KEY = "REPLACE_ME"
+USE_TZ = True
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db_setup")
 from django.core.wsgi import get_wsgi_application
